6c5843594b1f638c727b185160effa216e7a216b
avihadma <avihadma@post.jce.ac.il>
2012-12-05 14:21:49 +0200

    green

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -10,7 +10,6 @@ public class RomanNumeralUnitTests {
 	@Test
 	public void testThat1ReturnsI() {
 		assertEquals("I", number.toRoman(1));
-		assertEquals("II", number.toRoman(2));
 	}
 	
 
--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,9 +2,23 @@
 public class RomanNumerals {
 
 	public String toRoman(int nmber) {
-		if (nmber == 1)
-			return "I";
-		return "II";
+		
+		return binaryToRoman(nmber);
 	}
+	public static String binaryToRoman(int binary) {
+		String[] RCODE = {"M", "CM", "D", "CD", "C", "XC", "L","XL", "X", "IX", "V","IV","I"};
+		int[]    BVAL  = {1000, 900, 500, 400,  100,   90,  50, 40,   10,    9,   5,   4,  1};
+		if (binary <= 0 || binary >= 3000) {
+			System.out.println("Value outside roman numeral range.");
+		}
+		String roman = "";         
+		for (int i = 0; i < RCODE.length; i++) {
+			while (binary >= BVAL[i]) {
+				binary -= BVAL[i];
+				roman  += RCODE[i];
+			}
+		}
+		return roman;
+	}  
 
 }
